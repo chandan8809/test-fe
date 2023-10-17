1fed06cb5a43f34a6ec77779a4ed3e635adc5025
Chandan kumar <chandan.kumar@apnamart.in>
2023-10-17 18:57:14 +0530

    dvh added

--- src/components/testWindow/QuestionPallet.jsx
+++ src/components/testWindow/QuestionPallet.jsx
@@ -99,7 +99,7 @@ const QuestionPallet = ({
                 </div>
             </div>
 
-           <div className='flex flex-wrap gap-3 p-4 h-[calc(100vh-355px)] overflow-scroll'>
+           <div className='flex flex-wrap gap-3 p-4 h-[calc(100dvh-355px)] overflow-scroll'>
                 {selectedSection.questionList.map((each)=>{
                     // const targetQuestion=selectedSection.questionList.find((ques)=>ques._id===selectedQuestion._id)
                     // selectedQuestion.status="notVisited"
