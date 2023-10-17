ec2bed79718df90110422b7bbe12dc10521bd89c
Chandan kumar <chandan.kumar@apnamart.in>
2023-10-17 18:38:05 +0530

    scrollalbe top

--- src/components/testWindow/MobileHeader.jsx
+++ src/components/testWindow/MobileHeader.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 import SegmentIcon from '@mui/icons-material/Segment';
-import IconButton from '@mui/material/IconButton';
+
 import Countdown from 'react-countdown';
+import SubjectIcon from '@mui/icons-material/Subject';
 
 const MobileHeader = ({toggleDrawer}) => {
   return (
     <main className=' bg-black flex justify-between items-center py-1'>
-        <div className='left pl-3 flex items-center gap-2'>
-            <PauseCircleOutlineIcon sx={{color:"white"}}/>
+        <div className='left pl-5 flex items-center gap-2'>
+            <PauseCircleOutlineIcon  sx={{color:"white", height:"30px", width:"30px"}}/>
 
             <div className='text-white'>
                 <Countdown date={Date.now() + 1000000}  className='font-semibold'/>
@@ -16,9 +17,9 @@ const MobileHeader = ({toggleDrawer}) => {
             </div>
         </div>
         
-        <IconButton aria-label="delete" onClick={toggleDrawer("right",true)}>
-            <SegmentIcon sx={{color:"white"}}/>
-        </IconButton>
+      
+        <SegmentIcon onClick={toggleDrawer("right",true)} sx={{color:"white", height:"40px", width:"60px"}}/>
+     
        
     </main>
   )
