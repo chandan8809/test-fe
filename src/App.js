7af0c405e8c210cdb00e66836f455c0c8549f673
Chandan kumar <chandan.kumar@apnamart.in>
2023-10-17 14:48:08 +0530

    making responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import MainWindow from './components/testWindow/MainWindow';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function App() {
+  const matches = useMediaQuery('(min-width:600px)');
+
   return (
     <div className="App">
       <CssBaseline />
       <MainWindow/>
-     
     </div>
   );
 }
--- src/components/testWindow/MainWindow.jsx
+++ src/components/testWindow/MainWindow.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Section from './Section'
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 const MainWindow = () => {
+  const matches = useMediaQuery('(min-width:600px)');
   return (
     <div>
-        <Header/>
+        {matches && <Header/>}
         <Section/>
     </div>
   )
