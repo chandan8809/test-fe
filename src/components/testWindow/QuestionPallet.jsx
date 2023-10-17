0ecb7bf945520ed36fd2bcdc02234e7ae956c7a6
Chandan kumar <chandan.kumar@apnamart.in>
2023-10-17 20:51:17 +0530

    hello

--- src/components/testWindow/QuestionPallet.jsx
+++ src/components/testWindow/QuestionPallet.jsx
@@ -99,7 +99,7 @@ const QuestionPallet = ({
                 </div>
             </div>
 
-           <div className={`flex flex-wrap gap-3 p-4 overflow-scroll  ${matches ? "h-[calc(100vh-292px)]":"h-[calc(100vh-355px)]"} `}>
+           <div className={`flex flex-wrap gap-3 p-4 overflow-scroll  ${matches ? "h-[calc(100dvh-292px)]":"h-[calc(100dvh-355px)]"} `}>
                 {selectedSection.questionList.map((each)=>{
                     // const targetQuestion=selectedSection.questionList.find((ques)=>ques._id===selectedQuestion._id)
                     // selectedQuestion.status="notVisited"
